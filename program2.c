426f7305008c7c251499c042b73ee1a4657a1852
bogdangoz78 <bogdan78@wp.pl>
2017-10-19 18:01:07 +0200

    dwa kolejne programy

---
+++ program2.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main()
+{
+
+		putchar('v'); /*wpisz znak */
+		putchar('\n'); /*znak konca linii*/	
+	puts("wypisz string / ciag znaków");
+	puts("\nAla ma kota,\na kot ma Ale");
+	return 0; /* main zwraca wartosc 0*/
+}
+
+
+
---
+++ program3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int dodaj(int a, int b)
+int tesr_czy_dobrze_dodaje(int arg1, int arg2, int wynik)
+{
+tf(
+
+	return a+b;
+}
+int main()
+{ 
+	printf("%d", dodaj(4,5));
+
+	return 0;
+}
